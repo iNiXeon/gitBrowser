daf27407df084ab6463cd96ce68b7b1d84593763
NiXeon <mixeon@gmail.com>
2022-02-02 10:50:09 +0500

    Changes to be committed:

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div id="title">Dashboard
+    <div>
+      <div id="title">Dashboard</div>
       <Link to="/dashboard/profile/5b7a818e-af0f-4f2c-bba0-21b400fe5cbc">Go To Profile</Link>
       <Link to="/dashboard/main">Go To Main</Link>
     </div>
